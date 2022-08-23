87f25c020b8b8630eaffbf08b4d65edc8f075ebf
IDDna <romanov1371@gmail.com>
2022-08-23 13:48:34 +0300

    Insert data in DOM

--- src/index.js
+++ src/index.js
@@ -2,9 +2,21 @@ import './styles/main.scss';
 import getWeatherData from './getWeatherData';
 import setWeatherIcon from './setWeatherIcon';
 
-async function runApp() {
-  const weatherData = await getWeatherData('Moscow');
+async function runApp(country) {
+  const tempParam = document.querySelector('.temp span');
+  const descParam = document.querySelector('.desc');
+  const pressureParam = document.querySelector('#pressure');
+  const humidityParam = document.querySelector('#humidity');
+  const windParam = document.querySelector('#wind');
+
+  const weatherData = await getWeatherData(country);
   setWeatherIcon(weatherData.icon);
+
+  tempParam.textContent = Math.round(weatherData.temp);
+  descParam.textContent = weatherData.desc;
+  pressureParam.textContent = weatherData.pressure + 'hpa';
+  humidityParam.textContent = weatherData.humidity + '%';
+  windParam.textContent = weatherData.windSpeed + 'm/s';
 }
 
-runApp();
+runApp('Moscow');
